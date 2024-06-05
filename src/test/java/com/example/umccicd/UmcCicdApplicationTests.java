5dc7b62b873c5986a6efb1ad1f98cfc1155087dc
rjswjddn <rjswjddn@gmail.com>
2024-06-05 18:33:04 +0900

    first commit

---
+++ src/test/java/com/example/umccicd/UmcCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umccicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
